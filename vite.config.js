76a0cee784c71fa4563113ed2dbaa381f48f689b
Ondreev <mrtutanhamon@gmail.com>
2025-07-07 20:07:52 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
-// vite.config.js
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: '.', // указывает, что корень проекта — текущая папка
+  plugins: [react()],
+  root: '.', // корень проекта
+  build: {
+    outDir: 'dist',
+  },
 })
