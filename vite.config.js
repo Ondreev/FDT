7c25fb0bd590d9e3733ed62a19344a835a62163b
Ondreev <mrtutanhamon@gmail.com>
2025-07-07 20:31:00 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html')
-      }
-    }
-  }
+  root: '.', // ⬅️ Это важно: говорим Vite, что корень проекта — текущая папка
 })
