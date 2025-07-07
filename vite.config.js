e6e7e481243ddf251e23d20d78bc969d9a0da26d
Ondreev <mrtutanhamon@gmail.com>
2025-07-07 20:12:24 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  root: '.', // корень проекта
   build: {
-    outDir: 'dist',
-  },
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
+    }
+  }
 })
+
