287ac4acb8c0ae38706eff2cc7d6aba0253c8570
Ondreev <mrtutanhamon@gmail.com>
2025-07-08 16:43:26 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  root: '.', // ⬅️ Это важно: говорим Vite, что корень проекта — текущая папка
+  plugins: [react()]
 })
