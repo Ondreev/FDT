6a7b1a0db7063cbdaa950d5cf769302b049492bb
Ondreev <mrtutanhamon@gmail.com>
2025-07-09 16:10:18 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,11 @@ export default defineConfig({
   plugins: [react()],
   base: '/FDT/', // <-- имя твоего репозитория на GitHub
 })
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/FDT/', // <-- важно: название репозитория!
+})
