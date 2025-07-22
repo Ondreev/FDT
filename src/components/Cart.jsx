6bfc743d44d963f02d894e6bff0ffcae3534e06d
Ondreev <mrtutanhamon@gmail.com>
2025-07-22 18:09:21 +0300

    Update Cart.jsx

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import DiscountProgressBar from './DiscountProgressBar';
 import FlashOfferTimer from './FlashOfferTimer';
 
-const API_URL = 'https://script.google.com/macros/s/AKfycbxIz5qxFXEc3vW4TnWkGyZAVA4Y9psWkvWXl7iR5V_vyyAT-fsmpGPGInuF2C3MIw427w/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbytJQZKK_57WXTalemzNQgVmlcS_HajnL0vo-FxDT4DogOCLDnJ4vWl0GMp8oQCaOi0/exec';
 
 const Cart = ({ isOpen, onClose, cart, updateQuantity, removeFromCart, settings, addToCart, onOpenOrderForm }) => {
   const [discounts, setDiscounts] = useState([]);
