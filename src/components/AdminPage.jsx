605e940369775b3847025bebe6cbec6c9c7116c5
Ondreev <mrtutanhamon@gmail.com>
2025-07-23 17:07:01 +0300

    Update AdminPage.jsx

--- src/components/AdminPage.jsx
+++ src/components/AdminPage.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-const API_URL = 'https://script.google.com/macros/s/AKfycbwpgkiVZN5JwPdSYj-jLVZHZ_A5sw8P6PV4QXR7DJWchwP-19z31WUjcv7QRaHMAazCxg/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbzXamimf6YHIrXtbn_NKNCGATgt3wRNuly1HQlOhbm3tcUyygLXoNvRDyDRyFTAIsxuRw/exec';
 
 const formatNumber = (num) => {
   return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
