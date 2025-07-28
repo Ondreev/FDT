108fd1ae386fb6f8e5d4d2d483325abd478d2dfc
Ondreev <mrtutanhamon@gmail.com>
2025-07-28 20:32:35 +0300

    Update AdminLogin.jsx

--- src/components/AdminLogin.jsx
+++ src/components/AdminLogin.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 
-const API_URL = 'https://script.google.com/macros/s/AKfycbxAQF0sfNYonRjjH3zFBW58gkXZ3u5mKZWUtDyspY3uyHxFc-WnZB13Hz8IH1w-h3bG2Q/exec';
+import { API_URL } from '../config'; // или './config' в зависимости от расположения
 
+// И использовать API_URL вместо прямой ссылки
 const AdminLogin = ({ onLoginSuccess }) => {
   const [password, setPassword] = useState('');
   const [isLoading, setIsLoading] = useState(false);
